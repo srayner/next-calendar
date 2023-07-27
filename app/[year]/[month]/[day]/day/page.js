252513e4bb0ad463df1abbc043c9b4bab0fa1c98
Steve Rayner <srayner02@gmail.com>
2023-07-27 22:52:17 +0100

    added hours to day view

--- app/[year]/[month]/[day]/day/page.js
+++ app/[year]/[month]/[day]/day/page.js
@@ -1,14 +1,37 @@
 'use client'
 
 import WeekDays from '../../../../components/week-days/WeekDays';
+import styles from './days.module.css';
 
 const DayPage = (props) => {
 
     const { year, month, day } = props.params;
     const currentDate = new Date(year, month - 1, day);
 
+    const hours = [];
+    for (let i = 0; i < 24; i++) {
+        hours.push(i.toString().padStart(2, '0') + ':00');
+    }
+
     return (
-        <WeekDays date={currentDate} singleDay/>
+        <div className={styles.dayView}>
+            <WeekDays date={currentDate} singleDay/>
+            <div className={styles.hours}>
+                <div className={styles.topRow} key="-1">
+                    <div className={styles.hour}></div>
+                    <div className={styles.hourLine}></div>
+                    <div className={styles.hourContainer}></div>
+                </div>
+
+                {hours.map((hour, index) => (
+                    <div className={styles.row} key={index}>
+                        <div className={styles.hour}>{hour}</div>
+                        <div className={styles.hourLine}></div>
+                        <div className={styles.hourContainer}></div>
+                    </div>
+                ))}
+            </div>
+        </div>
     );
 
   }
