75fa7794653759abf405cc61fb9e9ab9f48cbfce
Steve Rayner <srayner02@gmail.com>
2023-08-13 19:50:22 +0100

    updated formating for titlebar and sidebar components

--- app/components/side-bar/SideBar.js
+++ app/components/side-bar/SideBar.js
@@ -1,62 +1,64 @@
-'use client'
+"use client";
 
-import Calendar from '../calendar/Calendar';
-import styles from './side-bar.module.css'
-import { useSelectedLayoutSegments } from 'next/navigation';
-import { useRouter } from 'next/navigation';
-import { addHours, format, roundToNearestMinutes, startOfDay } from 'date-fns';
-import DropDown from '../drop-down/DropDown';
-import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
-import { ModalContext } from '@/app/layout';
-import { useContext } from 'react';
+import Calendar from "../calendar/Calendar";
+import styles from "./side-bar.module.css";
+import { useSelectedLayoutSegments } from "next/navigation";
+import { useRouter } from "next/navigation";
+import { addHours, format, roundToNearestMinutes, startOfDay } from "date-fns";
+import DropDown from "../drop-down/DropDown";
+import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
+import { ModalContext } from "@/app/layout";
+import { useContext } from "react";
 
 const SideBar = () => {
+  const router = useRouter();
+  const { openModal } = useContext(ModalContext);
 
-    const router = useRouter();
-    const { openModal } = useContext(ModalContext);
+  const now = new Date();
 
+  const segments = useSelectedLayoutSegments();
+  const viewName = segments[segments.length - 1];
+
+  const handleDateChange = (date) => {
+    const url = "/" + format(date, "yyyy/MM/dd") + "/" + viewName;
+    router.push(url);
+  };
+
+  const handleCreate = (type) => {
     const now = new Date();
+    const start = roundToNearestMinutes(now, {
+      nearestTo: 15,
+      roundingMethod: "ceil",
+    });
+    const end = addHours(start, 1);
+    const newEvent = {
+      type,
+      name: "",
+      start: start,
+      end: end,
+      colour: "#039be5",
+    };
+    openModal(newEvent);
+  };
 
-    const segments = useSelectedLayoutSegments();
-    const viewName = segments[segments.length - 1];
+  const items = [
+    { caption: "Event", shortcut: "E", value: "event" },
+    { caption: "Task", shortcut: "T", value: "task" },
+  ];
 
-    const handleDateChange = (date) => {
-        const url = '/' + format(date, 'yyyy/MM/dd') + '/' + viewName;
-        router.push(url);
-    }
-
-    const handleCreate = (type) => {
-        const now = new Date();
-        const start = roundToNearestMinutes(now, {nearestTo: 15, roundingMethod: 'ceil'});
-        const end = addHours(start , 1);
-        const newEvent = {
-            type,
-            name: '',
-            start: start,
-            end: end,
-            colour: '#039be5'
-        }
-        openModal(newEvent);
-    }
-
-    const items = [
-        { caption: 'Event', shortcut: 'E', value: 'event'},
-        { caption: 'Task', shortcut: 'T', value: 'task'}
-    ];
-
-    return (
-        <div className={styles.sideBar}>
-            <DropDown
-                type={'primary'}
-                items={items}
-                caption="Create"
-                icon={faCaretDown}
-                menuAligned="left"
-                onSelected={(data) => handleCreate(data)}
-                />
-            <Calendar startingMonth={now} onSelect={handleDateChange} showNav/>
-        </div>
-    )
-}
+  return (
+    <div className={styles.sideBar}>
+      <DropDown
+        type={"primary"}
+        items={items}
+        caption="Create"
+        icon={faCaretDown}
+        menuAligned="left"
+        onSelected={(data) => handleCreate(data)}
+      />
+      <Calendar startingMonth={now} onSelect={handleDateChange} showNav />
+    </div>
+  );
+};
 
 export default SideBar;
--- app/components/title-bar/TitleBar.js
+++ app/components/title-bar/TitleBar.js
@@ -1,121 +1,124 @@
-'use client'
+"use client";
 
-import Button from '../button/Button';
-import DropDown from '../drop-down/DropDown';
-import RoundButton from '../round-button/RoundButton';
-import Text from '../text/Text';
-import { faCaretDown, faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import Button from "../button/Button";
+import DropDown from "../drop-down/DropDown";
+import RoundButton from "../round-button/RoundButton";
+import Text from "../text/Text";
 import {
-    addYears,
-    addMonths,
-    addWeeks,
-    addDays,
-    endOfWeek,
-    format,
-    getMonth,
-    getYear,
-    startOfWeek,
-    subYears,
-    subMonths,
-    subWeeks,
-    subDays,
-} from 'date-fns';
-import styles from './title-bar.module.css';
-import { useParams } from 'next/navigation';
-import { useSelectedLayoutSegments } from 'next/navigation';
-import { useRouter } from 'next/navigation';
+  faCaretDown,
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  addYears,
+  addMonths,
+  addWeeks,
+  addDays,
+  endOfWeek,
+  format,
+  getMonth,
+  getYear,
+  startOfWeek,
+  subYears,
+  subMonths,
+  subWeeks,
+  subDays,
+} from "date-fns";
+import styles from "./title-bar.module.css";
+import { useParams } from "next/navigation";
+import { useSelectedLayoutSegments } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 const TitleBar = (props) => {
+  const router = useRouter();
 
-    const router = useRouter();
+  const { year, month, day } = useParams();
+  const segments = useSelectedLayoutSegments();
+  const viewName = segments[segments.length - 1];
 
-    const {year, month, day} = useParams();
-    const segments = useSelectedLayoutSegments();
-    const viewName = segments[segments.length - 1];
-    
-    if (typeof(year) === 'undefined') {
-        return;
-    }
-    
-    const currentDate = new Date(year, month - 1, day);
-    const today = new Date();
-    const todayLink = '/' + format(today, 'yyyy/MM/dd') + '/' + viewName;
+  if (typeof year === "undefined") {
+    return;
+  }
 
-    let prevLink;
-    let nextLink;
-    let dateText;
-    switch (viewName) {
-        case 'year':
-            prevLink = format(subYears(currentDate, 1), '/yyyy/MM/dd') + '/year';
-            nextLink = format(addYears(currentDate, 1), '/yyyy/MM/dd') + '/year';
-            dateText = format(currentDate, 'yyyy');
-            break;
-        case 'month':
-            prevLink = format(subMonths(currentDate, 1), '/yyyy/MM/dd') + '/month';
-            nextLink = format(addMonths(currentDate, 1), '/yyyy/MM/dd') + '/month';
-            dateText = format(currentDate, 'MMMM yyyy');
-            break;
-        case 'week':
-            prevLink = format(subWeeks(currentDate, 1), '/yyyy/MM/dd') + '/week';
-            nextLink = format(addWeeks(currentDate, 1), '/yyyy/MM/dd') + '/week';
-            const weekStart = startOfWeek(currentDate, { weekStartsOn: 1 });
-            const weekEnd = endOfWeek(currentDate, { weekStartsOn: 1 });
-            if (getYear(weekStart) === getYear(weekEnd)) {
-                if (getMonth(weekStart) === getMonth(weekEnd)) {
-                    dateText =
-                        format(weekEnd, 'MMMM') + ' ' +
-                        format(currentDate, 'yyyy');
-                } else {
-                    dateText =
-                        format(weekStart, 'MMM') + ' - ' +
-                        format(weekEnd, 'MMM') + ' ' +
-                        format(currentDate, 'yyyy');
-                }
-            } else {
-                dateText =
-                    format(weekStart, 'MMM yyyy') + ' - ' +
-                    format(weekEnd, 'MMM yyyy');
-            }
-            break;
-        default:
-            prevLink = format(subDays(currentDate, 1), '/yyyy/MM/dd') + '/day';
-            nextLink = format(addDays(currentDate, 1), '/yyyy/MM/dd') + '/day';
-            dateText = format(currentDate, 'd MMMM yyyy');
-    }
+  const currentDate = new Date(year, month - 1, day);
+  const today = new Date();
+  const todayLink = "/" + format(today, "yyyy/MM/dd") + "/" + viewName;
 
-    const handleViewChange = (view) => {
-        router.push(`/${year}/${month}/${day}/${view}`);
-    }
+  let prevLink;
+  let nextLink;
+  let dateText;
+  switch (viewName) {
+    case "year":
+      prevLink = format(subYears(currentDate, 1), "/yyyy/MM/dd") + "/year";
+      nextLink = format(addYears(currentDate, 1), "/yyyy/MM/dd") + "/year";
+      dateText = format(currentDate, "yyyy");
+      break;
+    case "month":
+      prevLink = format(subMonths(currentDate, 1), "/yyyy/MM/dd") + "/month";
+      nextLink = format(addMonths(currentDate, 1), "/yyyy/MM/dd") + "/month";
+      dateText = format(currentDate, "MMMM yyyy");
+      break;
+    case "week":
+      prevLink = format(subWeeks(currentDate, 1), "/yyyy/MM/dd") + "/week";
+      nextLink = format(addWeeks(currentDate, 1), "/yyyy/MM/dd") + "/week";
+      const weekStart = startOfWeek(currentDate, { weekStartsOn: 1 });
+      const weekEnd = endOfWeek(currentDate, { weekStartsOn: 1 });
+      if (getYear(weekStart) === getYear(weekEnd)) {
+        if (getMonth(weekStart) === getMonth(weekEnd)) {
+          dateText =
+            format(weekEnd, "MMMM") + " " + format(currentDate, "yyyy");
+        } else {
+          dateText =
+            format(weekStart, "MMM") +
+            " - " +
+            format(weekEnd, "MMM") +
+            " " +
+            format(currentDate, "yyyy");
+        }
+      } else {
+        dateText =
+          format(weekStart, "MMM yyyy") + " - " + format(weekEnd, "MMM yyyy");
+      }
+      break;
+    default:
+      prevLink = format(subDays(currentDate, 1), "/yyyy/MM/dd") + "/day";
+      nextLink = format(addDays(currentDate, 1), "/yyyy/MM/dd") + "/day";
+      dateText = format(currentDate, "d MMMM yyyy");
+  }
 
-    const items = [
-        { caption: 'Year', shortcut: 'Y', value: 'year'},
-        { caption: 'Month', shortcut: 'M', value: 'month' },
-        { caption: 'Week', shortcut: 'W', value: 'week' },
-        { caption: 'Day', shortcut: 'D', value: 'day' }
-    ];
+  const handleViewChange = (view) => {
+    router.push(`/${year}/${month}/${day}/${view}`);
+  };
 
-    return (
-        <div className={styles.titleBar}>
-            <div className={styles.leftItems}>
-                <img src="/calendar.png" />
-                <div className={styles.title}>Calendar</div>
-                <Button key="1" type={'primary'} caption="Today" href={todayLink} />
-                <RoundButton icon={faChevronLeft} href={prevLink}/>
-                <RoundButton icon={faChevronRight} href={nextLink}/>
-                <Text>{dateText}</Text>
-            </div>
-            <div className={styles.rightItems}>
-                <DropDown
-                    type={'secondary'}
-                    items={items}
-                    selected={viewName}
-                    icon={faCaretDown}
-                    menuAligned="right"
-                    onSelected={(view) => handleViewChange(view)}
-                />
-            </div>
-        </div>
-    )
-}
+  const items = [
+    { caption: "Year", shortcut: "Y", value: "year" },
+    { caption: "Month", shortcut: "M", value: "month" },
+    { caption: "Week", shortcut: "W", value: "week" },
+    { caption: "Day", shortcut: "D", value: "day" },
+  ];
 
-export default TitleBar
+  return (
+    <div className={styles.titleBar}>
+      <div className={styles.leftItems}>
+        <img src="/calendar.png" />
+        <div className={styles.title}>Calendar</div>
+        <Button key="1" type={"primary"} caption="Today" href={todayLink} />
+        <RoundButton icon={faChevronLeft} href={prevLink} />
+        <RoundButton icon={faChevronRight} href={nextLink} />
+        <Text>{dateText}</Text>
+      </div>
+      <div className={styles.rightItems}>
+        <DropDown
+          type={"secondary"}
+          items={items}
+          selected={viewName}
+          icon={faCaretDown}
+          menuAligned="right"
+          onSelected={(view) => handleViewChange(view)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default TitleBar;
