c2216dff89db80e4aec46622626fa3f1c6d959c2
Steve Rayner <srayner02@gmail.com>
2023-08-27 17:04:01 +0100

    updated day view with the same functionality as week view for clicking to add/edit events

--- app/[year]/[month]/[day]/day/page.js
+++ app/[year]/[month]/[day]/day/page.js
@@ -6,20 +6,46 @@ import { format } from "date-fns";
 import { initHours } from "@/src/hours";
 import { populateHours } from "@/src/events";
 import { getEventsForDay } from "@/src/api";
-import styles from "./days.module.css";
 import { useEffect, useState } from "react";
+import { ModalContext } from "@/app/layout";
+import { useContext } from "react";
+import HourContainer from "@/app/components/hour-container/HourContainer";
+import styles from "./days.module.css";
 
 const DayPage = (props) => {
   const { year, month, day } = props.params;
   const currentDate = new Date(year, month - 1, day);
 
   const [events, setEvents] = useState([]);
+  const { openModal } = useContext(ModalContext);
 
   useEffect(() => {
     getEventsForDay(currentDate).then((eventsFromServer) => {
       setEvents(eventsFromServer);
     });
   }, []);
+
+  const eventUpdated = (updatedEvent) => {
+    setEvents((prevEvents) => {
+      const existingIndex = prevEvents.findIndex(
+        (event) => event.id === updatedEvent.id
+      );
+
+      if (existingIndex === -1) {
+        return [...prevEvents, updatedEvent];
+      }
+
+      return [
+        ...prevEvents.slice(0, existingIndex),
+        updatedEvent,
+        ...prevEvents.slice(existingIndex + 1),
+      ];
+    });
+  };
+
+  const editEvent = (event) => {
+    openModal(event, eventUpdated);
+  };
 
   const days = [currentDate];
   const hours = initHours(days);
@@ -40,11 +66,15 @@ const DayPage = (props) => {
           <div className={styles.row} key={index}>
             <div className={styles.hour}>{hour.name}</div>
             <div className={styles.hourLine}></div>
-            <div className={styles.hourContainer}>
+            <HourContainer
+              name={hour.name}
+              day={currentDate}
+              onClick={editEvent}
+            >
               {hour[format(currentDate, "EEE")].events.map((event, index) => (
-                <Event event={event} key={index} />
+                <Event event={event} key={index} onClick={editEvent} />
               ))}
-            </div>
+            </HourContainer>
           </div>
         ))}
       </div>
